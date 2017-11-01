33cadf0292f984791d630c45569f356c3b45df2b
Carlton Joseph <carlton.joseph@gmail.com>
2017-10-31 23:14:12 -0700

    add recompose branch

--- src/app/App.js
+++ src/app/App.js
@@ -9,12 +9,14 @@ import Enums from '../enums/enums';
 import HocNot from '../hoc/hocNot';
 import Hoc from '../hoc/hoc';
 import Hoc2 from '../hoc/hoc2';
+import Hoc3 from '../hoc/hoc3';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header"> <h1 className="App-title">Testing Space</h1> </header>
+        <Hoc3 />
         <Hoc2 />
         <Hoc />
         <HocNot />
---
+++ src/hoc/hoc3.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+  branch,
+  compose,
+  renderComponent,
+  renderNothing,
+} from 'recompose';
+
+import { Spinner } from '../utils/styles';
+
+export const Hoc2List = ({ list }) => {
+  return (<ul>{list.map((item,index) => <li key={index}>{item}</li>)}</ul>);
+};
+
+const withConditionalRenderings = compose(
+  branch(({isLoading}) => isLoading,
+    renderComponent(() => <Spinner></Spinner>),
+    branch((props) => !props.list,
+      renderNothing,
+      branch((props) => !props.list.length,
+        renderComponent(() => <div><p>No items in the list</p></div>),
+      ),
+    ),
+  ),
+);
+
+const Hoc2ListWithConditionalRendering = withConditionalRenderings(Hoc2List);
+
+class Hoc3Component extends Component {
+  state = {
+    list: null,
+    isLoading: true,
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState(state => ({ ...state, isLoading:  false }));
+      setTimeout(() => {
+          this.setState(state => ({ ...state, list: []}));
+        setTimeout(() => {
+          this.setState(state => ({ ...state, list: ['a','b'] }));
+          setTimeout(() => {
+            this.setState(state => ({ ...state, list: [...state.list, 'c','d']}));
+          }, 700);
+        }, 700);
+      }, 700);
+    }, 700);
+  }
+
+  render() {
+    return (<div>
+      <h1>HOC 3</h1>
+      <Hoc2ListWithConditionalRendering { ...this.state } />
+    </div>);
+  }
+}
+
+export default Hoc3Component;
