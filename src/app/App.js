d5a08c3d7e1263349f65e74f2e1bb315b5968969
Carlton Joseph <carlton.joseph@gmail.com>
2017-11-01 00:25:31 -0700

    add recompose example page

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import If from '../if/if';
 import Ternary from '../ternary/ternary';
 import Logic from '../logic/logic';
-import Switch from '../switch/switch';
+import SwitchCj from '../switch/switch';
 import Enums from '../enums/enums';
 import HocNot from '../hoc/hocNot';
 import Hoc from '../hoc/hoc';
 import Hoc2 from '../hoc/hoc2';
 import Hoc3 from '../hoc/hoc3';
+import P1recompose from '../pages/page1recompose';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header"> <h1 className="App-title">Testing Space</h1> </header>
-        <Hoc3 />
-        <Hoc2 />
-        <Hoc />
-        <HocNot />
-        <Enums />
-        <Switch />
-        <Logic />
-        <Ternary />
-        <If />
+        <Switch>
+          <Route path="/page1" render={() => (
+            <P1recompose />
+          )}/>
+          <Route render={() => (<div>
+            <Hoc3 />
+            <Hoc2 />
+            <Hoc />
+            <HocNot />
+            <Enums />
+            <SwitchCj />
+            <Logic />
+            <Ternary />
+            <If />
+          </div>)} />
+        </Switch>
       </div>
     );
   }
---
+++ src/pages/page1recompose.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  compose,
+  branch,
+  defaultProps,
+  flattenProp,
+  mapProps,
+  renameProps,
+  renderComponent,
+  withHandlers,
+  withProps,
+  withState,
+} from 'recompose';
+
+const enhance = compose (
+  defaultProps({
+    counter: 3,
+  }),
+  renameProps({
+    counter: "counter2",
+  }),
+  withProps({
+    employee: {name: "Carlton", id: 11}
+  }),
+  withState('cntr', 'updateCounter', 0),
+  withHandlers({
+    incr: ({updateCounter}) => () => updateCounter(c => c + 1),
+    decr: ({updateCounter}) => () => updateCounter(c => c - 1),
+    zero: ({updateCounter}) => () => updateCounter(c => 0),
+  }),
+  mapProps(({employee, ...rest}) => {
+    return {
+      ...rest,
+      employee: {
+        ...employee,
+        name: employee.name.toUpperCase()
+      }
+    }
+  }),
+  flattenProp('employee'),
+  branch(
+    ({id}) => id < 10,
+    renderComponent(Under40)
+  )
+);
+
+function App({counter2, name, id, cntr, updateCounter,
+  incr, decr, zero, ...others}) {
+  return (
+    <div>
+      <h1>Hello App Functions</h1>
+      <p>Counter: {counter2}</p>
+      <p>
+        Cntr: {cntr} &nbsp;
+        <button onClick={() => updateCounter(c => c + 1)}>+</button>
+        <button onClick={() => updateCounter(c => c - 1)}>-</button>,
+        <button onClick={incr}>+</button>
+        <button onClick={decr}>-</button>
+        <button onClick={zero}>0</button>
+      </p>
+      <p>Employee: {name}, ID: {id}</p>
+    </div>
+  );
+}
+
+function Under40({name, id}) {
+  return (<div>
+      <h1>Hey you are one of the early ones.</h1>
+  </div>);
+}
+
+export default enhance(App);
